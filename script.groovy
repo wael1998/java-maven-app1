af6639ad4a2dee9e68b7d9d8baad97e085fa6bd3
wael1998 <wael.chakchouk@esprit.tn>
2022-08-27 23:25:11 +0100

    push  to nexus repository

--- script.groovy
+++ script.groovy
@@ -6,14 +6,22 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t wael98/demo-app:jma-2.0 .'
+        sh 'docker build -t wael98/demo-app:jma-2.5 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push wael98/demo-app:jma-2.0'
+        sh 'docker push wael98/demo-app:jma-2.5'
     }
 } 
 
 def deployApp() {
-    echo 'deploying the application...'
+     
+     echo "deploying the application"
+     withCredentials([usernamePassword(credentialsId:'nexus-docker-repo',usernameVariable:'USER',passwordVariable:'PWD')]) {
+        sh "echo $PWD | docker login -u $USER --password-stdin localhost:8083"
+        sh "docker build -t localhost:8083/java-maven-app:2.5 ."
+        sh "docker push localhost:8083/java-maven-app:2.5"
+    }
+              
+          
 } 
 
 return this
