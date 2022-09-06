68327107613472ba0038f883b661e613360ab7e9
wael1998 <wael.chakchouk@esprit.tn>
2022-09-06 21:34:48 +0100

    change tag

--- script.groovy
+++ script.groovy
@@ -6,21 +6,21 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t wael98/demo-app:jma-2.5 .'
+        sh 'docker build -t wael98/demo-app:jma-3.5 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push wael98/demo-app:jma-2.5'
+        sh 'docker push wael98/demo-app:jma-3.5'
     }
 } 
 
 def deployApp() {
      
      echo "deploying the application"
-     withCredentials([usernamePassword(credentialsId:'nexus-docker-repo',usernameVariable:'USER',passwordVariable:'PWD')]) {
+     /*withCredentials([usernamePassword(credentialsId:'nexus-docker-repo',usernameVariable:'USER',passwordVariable:'PWD')]) {
         sh "echo $PWD | docker login -u $USER --password-stdin localhost:8083"
         sh "docker build -t localhost:8083/java-maven-app:2.5 ."
         sh "docker push localhost:8083/java-maven-app:2.5"
     }
-              
+        */      
           
 } 
 
